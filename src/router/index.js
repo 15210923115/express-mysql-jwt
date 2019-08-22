15a7dffdb47cc86ca494fdffcfa362ebb4ef4366
yangyang01 <yangyang01@iyunxiao.com>
2019-08-22 10:16:52 +0800

    gitignore

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-function router(app) {
+async function router(app) {
     let dirpath = path.resolve(__dirname);
-    fs.readdir(dirpath,(err, files) => {
+    await fs.readdir(dirpath,(err, files) => {
         files.forEach((item) => {
             if (item != 'index.js') {
                 let data = fs.readFileSync(path.resolve(__dirname, item), 'utf-8' , 'r');
